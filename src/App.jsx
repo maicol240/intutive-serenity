1f9fc8699aaa08dd37e29b22286b510abedb74ee
Maicol <72515186+maicol240@users.noreply.github.com>
2025-12-16 03:18:39 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import "./App.css";
+import { Header, Hero } from "./components/index.js";
+
+function App() {
+  return <></>;
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+export { default as Header } from "./Header/Header.jsx";
+export { default as Hero } from "./Hero/Hero.jsx";
